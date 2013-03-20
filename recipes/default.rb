2fa36a93d9a4d5ecac31a9c099d9e761b2e5ea4f
Steven Yang <yangchenyun@gmail.com>
2013-03-20 12:21:47 +0800

    updated the apt source mannually after settings

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,3 +13,7 @@ cookbook_file "/etc/apt/sources.list" do
   action :create
   notifies :run, "execute[apt-get-update]", :immediately
 end
+
+execute 'update source' do
+  command "apt-get update"
+end
